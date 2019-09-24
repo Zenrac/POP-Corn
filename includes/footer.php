5e217e51c0bbe0d37c46838e1621a176ad5e81ce
EnzoCnz <ecarnez.sio@gmail.com>
2019-09-24 15:08:57 +0200

    Initialisation des scripts, ajout du header et du footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,37 @@
-<footer>
+<?php
+	echo "<footer class=" . ((isset($_POST['table'])) ? "" : "fixed") . ">";
+?>
+
+	<div class=infos>
+
+		<div class=quisommenous>
+			<h4>
+				Qui sommes nous ?
+			</h4>
+			<h5>
+				Nous sommes une société d'achat et de revente de véhicule de haute qualité. Nous sommes 100% fiable et possédons beaucoup d'argent. Si vous lisez encore ceci je vous conseille d'arrêter sauf si vous travaillez pour le FBI.
+			</h5>
+		</div>
+		<div class=information>
+			<h4>
+				Informations
+			</h4>
+			<h5>
+				Nos produits de hautes qualités et performance vous permetrons de faire des pâtes, des gateaux, des tapisseries et accesoirement la pointe de la pérformance aéro-spaciale en therme de véhicule d'exeption miniature.
+			</h5>
+		</div>
+		<div class=nouscontacter>
+			<h4>
+				Nous contacter
+			</h4>
+			<h5>
+				Vous ne pouvez pas nous contacter car on n'existe pas. Mais bon, après vous pouvez toujours essayer. J'aurai pu utiliser Lorem Ipsum pour combler ce vide de texte, mais j'ai préféré perdre du temps à écrire cette phrase.
+			</h5>
+		</div>
+	</div>
+
+	<h3>
+		Meilleur site de l'univers by Enzo CARNEZ.
+	</h3>
 
 </footer>
