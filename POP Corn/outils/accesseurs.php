f0605fdcfcb50dbab37eb0b57a616ee3a2e93336
EnzoCnz <ecarnez.sio@gmail.com>
2019-09-30 12:39:58 +0200

    Error

--- POP Corn/outils/accesseurs.php
+++ POP Corn/outils/accesseurs.php
@@ -1,12 +1,17 @@
 <?php
-function get_path($path)
-{
-	return dirname(__DIR__).'/'."$path";
-}
+	function get_page($name)
+	{
+		return (basename($_SERVER['PHP_SELF']) === "index.php" && "$name" != "index.php") ? 'pages/'."$name" : "$name";
+	}
 
 	function get_relative_path($path)
 	{
 		return (basename($_SERVER['PHP_SELF']) === "index.php") ? "$path" : '../'."$path";
+	}
+
+	function get_path($path)
+	{
+		return dirname(__DIR__).'/'."$path";
 	}
 
 	function getRelativePath($path) {
