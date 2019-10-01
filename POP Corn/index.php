e780e7d821bd21f904dd654056cd4ad32053c74a
Burlyparagon <laurentmartinez.sio@gmail.com>
2019-10-01 09:33:04 +0200

    modification de l'index, creation des pages info et mention legal

--- POP Corn/index.php
+++ POP Corn/index.php
@@ -1,31 +1,16 @@
 <!DOCTYPE html>
 <html>
+<?php
+	include_once './includes/header.php';
+?>
 	<body>
 
-		<?php
-			include_once './includes/header.php';
-		?>
 
-		<?php
-			include_once 'includes/footer.php';
-			echo getRelativePath('css/styles.css');
-		?>
+
+
 
 	</body>
+<?php
+	include_once '/includes/footer.php';
+?>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-	<body>
-
-		<?php
-			include_once './includes/header.php';
-		?>
-
-		<?php
-			include_once 'includes/footer.php';
-		?>
-
-	</body>
-</html>
->>>>>>> 2fa58a65701d3db537e22f3e6e9a48352055d04e
