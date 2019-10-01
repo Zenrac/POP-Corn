f85d07ae563a98969bd17c4df65be97c46cf48f0
EnzoCnz <ecarnez.sio@gmail.com>
2019-10-01 17:50:36 +0200

    Started spotify script

--- POP Corn/scripts/openform.js
+++ POP Corn/scripts/openform.js
@@ -1,23 +1,19 @@
-function openForm(id = 1) {
-  if (id != 1) {
-    current = document.querySelector('.connexadmin#inscription')
-    current.style.display = (current.style.display == "block") ? "none" : "block";
-  }
-  else {
-    current = document.querySelector('.connexadmin#connexion')
-    current.style.display = (current.style.display == "block") ? "none" : "block";
-  }
+function openForm() {
+  current = document.querySelector('.connexadmin#connexion')
+  current.style.display = (current.style.display == "block") ? "none" : "block";
 };
 
 function setInscription() {
   if (document.querySelector('#hiddeninput').value == '1') {
     document.querySelector('#admin-button button').textContent = 'Inscription';
     document.querySelector('#hiddeninput').value = '2';
-    document.querySelector('#switchinscription').textContent = "J'ai déjà un compte."
+    document.querySelector('#switchinscription').textContent = "J'ai déjà un compte.";
+    document.querySelector('#connexionbutton').value = "S'inscrire";
   }
   else {
     document.querySelector('#admin-button button').textContent = 'Connexion';
     document.querySelector('#hiddeninput').value = '1';
-    document.querySelector('#switchinscription').textContent = "Je n'ai pas de compte."
+    document.querySelector('#switchinscription').textContent = "Je n'ai pas de compte.";
+    document.querySelector('#connexionbutton').value = "Connexion";
   }
 }
--- POP Corn/scripts/spotify.js
+++ POP Corn/scripts/spotify.js
@@ -1,24 +1,48 @@
+get_token = "https://developer.spotify.com/console/get-search-item/?q=&type=&market=&limit=&offset="
+
 function fillDataBase() {
- 	var token = "d0df61c03b394843939d462426bcbc6a";
-	var top_url = "https://api.spotify.com/v1/playlists/3ZgmfR6lsnCwdffZUan8EA";
+
+  const api_url = "https://api.spotify.com/v1/playlists/";
+  const top_2019 = "0cYxgXN7MoBWTfVhv1D69A";
+  const top_2018 = "3pFAbyXA2ALOe79w3oIbaa";
+  const top_all = "3ZgmfR6lsnCwdffZUan8EA";
+
+  const token = "BQDWR6GrJsKDoPAiZroAt_oblNBP06iJSgK5ZXxS1yXtH8o_pzv65SyUCM4_RcqJfBkF0T3LRqFajqcEHJT3IurCNDPxyE1ZglZ1Ac3erIEJxPuv2ZwGHzGqhwFpbJsFEKJmBDUCJbsQKmJDqY_4Dpcb02X-vEC1fth1U0w";
 
 	let config = {
 	  headers: {
-	    authorization: token,
+      'Content-Type': 'application/json',
+	    authorization: 'Bearer ' + token
 	  }
 	}
 
-	axios.get(top_url, config)
-	.then(function (response) {
+  var url = api_url + top_2019 + "/tracks";
+  return axiosRequest(url, config);
+};
+
+function axiosRequest(url, config) {
+  return axios.get(url, config)
+  .then((response) => {
     // handle success
-    console.log(response);
+    return manageData(response.data);
   })
-  .catch(function (error) {
+  .catch((error) => {
     // handle error
-    console.log(error);
+    return error;
   })
-  .finally(function () {
+  .finally(() => {
     // always executed
   });
+}
 
-};
+function manageData(data) {
+  var userStr = JSON.stringify(data.items);
+  $.ajax({
+      url: '../index.php',
+      type: 'post',
+      data: {user: userStr},
+      success: function(response){
+          //do whatever.
+      }
+  });
+}
