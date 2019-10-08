0d29c48065e1b7cb5b18b2de3418b975acbb4908
AdranirYuri <rgot.sio@gmail.com>
2019-10-08 17:26:50 +0200

    ajout gestion tag et user dans barre back

--- POP Corn/includes/nav_back.php
+++ POP Corn/includes/nav_back.php
@@ -1,7 +1,19 @@
 <ul>
   <?php
     echo "<li><a href=".get_path('pages/back/indexAdmin.php').">Accueil</a></li>";
-    echo "<li><a href=".get_path('pages/back/inscriptionAdmin.php').">Inscription</a></li>";
+    echo '
+    <li>
+    <ul class="deroul">
+      <li>
+        <a href="#">Gestion</a>
+        <ul>
+        <li><a href='.get_path('pages/back/gestionUser.php').'>Utilisateur</a></li>
+        <li><a href='.get_path('pages/back/gestionTag.php').'>Tag</a></li>
+        </ul>
+      </li>
+    </ul>
+    </li>
+        ';
     echo "<li><a href=".get_path('index.php')." TARGET='_BLANK'>Site</a></li>";
     echo "<li><a href=".get_path('index.php?deconnexion=1')." name='deconnexion'>Deconnexion</a></li>";
   ?>
