6f2b793271796f2c3b8448237a64f04a2d12ae89
Rémy <rgot.sio@gmail.com>
2019-10-21 12:20:30 +0200

    Suppression de la musique de la playlist

--- POP Corn/pages/playlist.php
+++ POP Corn/pages/playlist.php
@@ -14,10 +14,29 @@
 				{
 					$duree = gmdate("i:s", $donnees['duree']/1000);
 
+					echo "<form action='".$_SERVER['PHP_SELF']."?id=".$_GET['id']."' method='post'>";
+					echo "<input type='submit' name='Supprimer' value='Supprimer' class='btt'></input>";
+					echo "	<input type='hidden' name='id' value='".$_GET['id']."' class='btt'></input>";
+					echo "	<input type='hidden' name='numMusique' value='".$donnees['numMusique']."' class='btt'></input>";
 					echo '<a href='.get_path('pages/music.php?id='.$donnees['numMusique']).'>'.$donnees['titre'].'</a>';
 					echo "  ";
 					echo "<input type='text' name='duree' value='".$duree."' size='4' readonly>";
+					echo "</form>";
 				}
+			}
+
+			if (!empty($_POST['Supprimer']))
+			{
+				$numMusique = "";
+				$id = "";
+				$numMusique = $cnx->quote($_POST['numMusique']);
+				$id = $cnx->quote($_POST['id']);
+				$req="	Delete from contenir where numMusique = ".$numMusique. " and numPlaylist = ".$id;
+				$cnx->exec($req);
+
+				echo "<script> location.assign(location.href);</script>";
+
+				$cnx = null;
 			}
         include_once '../includes/footer.php';
       ?>
