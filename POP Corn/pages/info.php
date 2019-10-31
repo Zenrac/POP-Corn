6a0dc781f8617b642e9b6a3bce40c9955c5f3de0
Burlyparagon <laurentmartinez.sio@gmail.com>
2019-10-31 13:56:06 +0100

    un petit bout de la page info

--- POP Corn/pages/info.php
+++ POP Corn/pages/info.php
@@ -5,6 +5,12 @@
 		<?php
 			include_once '../includes/header.php';
 		?>
+		<div class="nous">
+			<p>Hey nous sommes une grande equipe de 4 dévelopeurs profesionel en formation qui vous propose un repertoir d'un certain nombe de musique classer dans un ordre semi alleatoire. A vous de vous amuser à créer toutes les playlist dont vous révez.</p>
+
+		</div>
+
+
 
 		<?php
 			include_once '../includes/footer.php';
