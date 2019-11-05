c36a5175968136dbc8207b2c0bd28bbabcb4cb32
AdranirYuri <rgot.sio@gmail.com>
2019-11-05 17:26:25 +0100

    amélioration de la page recherche avance + message playlist vide

--- POP Corn/pages/playlist.php
+++ POP Corn/pages/playlist.php
@@ -9,19 +9,32 @@
 				$req="	SELECT titre, duree,  m.numMusique FROM contenir c inner join musique m on c.numMusique = m.numMusique
 				where numPlaylist =".$_GET['id']."  order by titre";
 				$req = $cnx->query($req);
-
-				while($donnees = $req->fetch(PDO::FETCH_ASSOC))
+				$elems = $req->fetchAll();
+				$nblignes = count($elems);
+				if ($nblignes == 0)
 				{
-					$duree = gmdate("i:s", $donnees['duree']/1000);
-
-					echo "<form action='".$_SERVER['PHP_SELF']."?id=".$_GET['id']."' method='post'>";
-					echo "<input type='submit' name='Supprimer' value='Supprimer' class='btt'></input>";
-					echo "	<input type='hidden' name='id' value='".$_GET['id']."' class='btt'></input>";
-					echo "	<input type='hidden' name='numMusique' value='".$donnees['numMusique']."' class='btt'></input>";
-					echo '<a href='.get_path('pages/music.php?id='.$donnees['numMusique']).'>'.$donnees['titre'].'</a>';
-					echo "  ";
-					echo "<input type='text' name='duree' value='".$duree."' size='4' readonly>";
+					echo "<form action='".$_SERVER['PHP_SELF']."' method='post'>";
+					echo "<span class='paramavance'>Il n'y a pas de musique dans la playlist</span><br>";
+					echo "<input type='submit' name='Playlist' value='Voir les musiques' class='btn btn-primary btn-block'></input>";
 					echo "</form>";
+
+				}
+				else
+				{
+					echo "<span class='paramavance'>Voici le résultat de la recherche :</span><br>";
+					foreach ($elems as $donnees)
+					{
+						$duree = gmdate("i:s", $donnees['duree']/1000);
+
+						echo "<form action='".$_SERVER['PHP_SELF']."?id=".$_GET['id']."' method='post'>";
+						echo "<input type='submit' name='Supprimer' value='Supprimer' class='btt'></input>";
+						echo "	<input type='hidden' name='id' value='".$_GET['id']."' class='btt'></input>";
+						echo "	<input type='hidden' name='numMusique' value='".$donnees['numMusique']."' class='btt'></input>";
+						echo '<a href='.get_path('pages/music.php?id='.$donnees['numMusique']).'>'.$donnees['titre'].'</a>';
+						echo "  ";
+						echo "<input type='text' name='duree' value='".$duree."' size='4' readonly>";
+						echo "</form>";
+					}
 				}
 			}
 
@@ -37,6 +50,11 @@
 				echo "<script> location.assign(location.href);</script>";
 
 				$cnx = null;
+			}
+
+			if (!empty($_POST['Playlist']))
+			{
+				echo "<script> location.assign('musiques.php');</script>";
 			}
         include_once '../includes/footer.php';
       ?>
