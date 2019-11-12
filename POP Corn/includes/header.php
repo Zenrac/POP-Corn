dce4799a150b1e974ae64400d664caf796c58992
ZenCz <ecarnez.sio@gmail.com>
2019-11-12 20:36:47 +0100

    cacher les erreurs php

--- POP Corn/includes/header.php
+++ POP Corn/includes/header.php
@@ -73,6 +73,8 @@
 
 <!--Verifier connexion-->
 <?php
+	error_reporting(0);
+	ini_set('display_errors', 0);
 	include_once(get_path('outils/connexpdo.inc.php'));
 	$cnx = connexpdo('bdpopcorn','myparam');
 	if ($cnx) {
